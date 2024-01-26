828303be5ee7ac823c22e550bff2f1894041cdd2
Renana Turgeman <renanaturgeman058@gmail.com>
2024-01-26 12:01:32 +0200

    Correcting the functions of the barriers

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
+import random
+
+
 def egalitarian_allocation(valuations: list[list[float]]):
     n = len(valuations[0])  # number of stuff
     states = [([0, 0], 0)]
     new_states = [([0, 0], 0)]
 
     while new_states:
+        # print("new_states", new_states)
+        # print("states", states)
         current_state, level = new_states.pop()
         if level < n:
             # in case that p1 get the stuff
             new_state_p1 = [current_state[0] + valuations[0][level], current_state[1]]
-            if ((new_state_p1, level + 1) not in states)  \
-                    and optimal_division(valuations, states.copy(), n) >= pessimistic_division(valuations, states.copy(), n):
+            if ((new_state_p1, level + 1) not in states) \
+                    and optimal_division(valuations, states.copy()) >= pessimistic_division(valuations, states.copy()):
                 states.append((new_state_p1, level + 1))
                 new_states.append((new_state_p1, level + 1))
 
             # in case that p2 get the staff
             new_state_p2 = [current_state[0], current_state[1] + valuations[1][level]]
             if (new_state_p2, level + 1) not in states \
-                    and optimal_division(valuations, states.copy(), n) >= pessimistic_division(valuations, states.copy(), n):
+                    and optimal_division(valuations, states.copy()) >= pessimistic_division(valuations, states.copy()):
                 states.append((new_state_p2, level + 1))
                 new_states.append((new_state_p2, level + 1))
                 # print(states)
@@ -28,28 +33,33 @@ def egalitarian_allocation(valuations: list[list[float]]):
     return max(filtered_states, key=lambda x: min(x[0]))
 
 
-def optimal_division(valuations: list[list[float]], states: [list[float], int], num: int) -> float:
+# in the optimal_division and the pessimistic_division I don't add the value the students already have, because this
+# amount will be reduced when compared
+def optimal_division(valuations: list[list[float]], states: [list[float], int]) -> float:
+    num = len(valuations[0])  # number of stuff
     current_state, level = states.pop()
-    sum1, sum2 = 0, 0
+    sum_value_p1, sum_value_p2 = 0, 0
     for stuff in range(num - level):
-        sum1 += valuations[0][stuff + level]
-        sum2 += valuations[1][stuff + level]
-    return min(sum1, sum2)
+        sum_value_p1 += valuations[0][stuff + level]
+        sum_value_p2 += valuations[1][stuff + level]
+    return min(sum_value_p1, sum_value_p2)
 
 
-def pessimistic_division(valuations: list[list[float]], states: [list[float], int], num: int) -> float:
+def pessimistic_division(valuations: list[list[float]], states: [list[float], int]) -> float:
+    num = len(valuations[0])  # number of stuff
     if not states:
         # Handle the case when the list is empty
         return 0
 
     current_state, level = states.pop()
-    min_value = float('inf')  # Initialize with positive infinity
-    for stuff in range(num - level):
-        if min_value > valuations[0][stuff + level]:
-            min_value = valuations[0][stuff + level]
-        if min_value > valuations[1][stuff + level]:
-            min_value = valuations[1][stuff + level]
-    return min_value
+    sum_value_p1, sum_value_p2 = 0, 0
+    for stuff in range(num - level):  # Go over the remaining stuff
+        rnd = random.randint(0, 10)  # for give the remaining stuff by random
+        if rnd < 5:  # If the number drawn is less than 5, the first student will receive it
+            sum_value_p1 += valuations[0][stuff + level]
+        else:  # else, the second student will receive it
+            sum_value_p2 += valuations[1][stuff + level]
+    return min(sum_value_p1, sum_value_p2)
 
 
 if __name__ == '__main__':
