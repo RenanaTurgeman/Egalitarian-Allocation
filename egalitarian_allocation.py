48ff0f84e96e4b7bb94deb938feae8b7d2d469da
Renana Turgeman <renanaturgeman058@gmail.com>
2024-01-28 17:50:12 +0200

    add comments

--- egalitarian_allocation.py
+++ egalitarian_allocation.py
@@ -43,18 +43,13 @@ def egalitarian_allocation(valuations: list[list[float]]):
                 states.append((new_state_p2, level + 1, current_state))
                 new_states.append((new_state_p2, level + 1, current_state))
 
-    print("all states: ", states)
-
     # An array that contains the final states in which all stuff were divided
     filtered_states = [state for state in states if state[1] == n]
     final_state = max(filtered_states, key=lambda x: min(x[0]))
-    print("final state:", final_state)
 
     path = find_parent(states, final_state)
-    print("path", path)
     dictionary = create_results(path)
     print_results(dictionary, final_state)
-    return final_state
 
 
 def find_parent(states, target_vector):
@@ -62,6 +57,7 @@ def find_parent(states, target_vector):
 
     for parent_state in states:
         if parent_state[0] == target_vector[2]:
+            # Recursively find the parent states
             result.extend(find_parent(states, parent_state))
 
     return [target_vector[0]] + result
@@ -70,36 +66,37 @@ def find_parent(states, target_vector):
 def create_results(path):
     results = {0: [], 1: []}
     staff = 0
-    path.reverse()
+    path.reverse() # for move from the root to the leaf
     for state, next_state in zip(path, path[1:]):
         if state[0] != next_state[0]:
-            # player 0 take the current object
+            # player 0 take the current staff
             results[0].append(staff)
         elif state[1] != next_state[1]:
-            # player 1 take the current object
+            # player 1 take the current staff
             results[1].append(staff)
         staff += 1
 
-    print("\nResults: ", results)
     return results
 
 
 def print_results(results, target):
     for player in results:
-        objects = ', '.join(map(str, results[player]))  # print the list without the [ ]
+        objects = ', '.join(map(str, results[player]))  # Convert the list to a string
         value = target[0][player]
         print(f"player {player} gets items {objects} with values {value}")
 
 
-# in the optimal_division and the pessimistic_division I don't add the value the students already have, because this
-# amount will be reduced when compared
 def optimal_division(valuations: list[list[float]], states: [list[float], int]) -> float:
     num = len(valuations[0])  # number of stuff
     current_state, level, parent_state = states.pop()
     sum_value_p1, sum_value_p2 = 0, 0
+
+    # Calculate the sum of values for both players
     for stuff in range(num - level):
         sum_value_p1 += valuations[0][stuff + level]
         sum_value_p2 += valuations[1][stuff + level]
+
+    # Return the minimum sum of values between the two players
     return min(sum_value_p1, sum_value_p2)
 
 
@@ -127,17 +124,3 @@ if __name__ == '__main__':
     # egalitarian_allocation([[4, 5], [8, 7]])
     # egalitarian_allocation([[6, 7, 9], [9, 7, 6]])
 
-    # valuations = [[4, 5], [8, 7]]
-    # result = egalitarian_allocation(valuations)  # ([5, 8], 2)
-    # print_allocation(result, valuations)
-    # print_graph()
-    # result = egalitarian_allocation([[4, 5], [8, 7]])
-    # path_to_root = trace_back_to_root(result)
-    #
-    # for idx, state in enumerate(path_to_root):
-    #     print(f"Step {idx + 1}: {state}")
-
-    # path = find_parent(
-    #     [([0, 0], 0, None), ([4, 0], 1, [0, 0]), ([0, 8], 1, [0, 0]), ([5, 8], 2, [0, 8]), ([0, 15], 2, [0, 8]),
-    #      ([9, 0], 2, [4, 0])], ([5, 8], 2, [0, 8]))
-    # print(path)
